b688c7b42019398338ec3d8333c5f1de053c9834
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-30 01:16:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -84,11 +84,24 @@ class Dog
     binding.pry
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
-      dog = Dog.new(name: name, breed: breed)
+      dog_data = Dog.new(name: name, breed: breed)
     else
       
     end
     dog
+    
+    
   end
+  
+    def self.find_or_create_by(name:, album:)
+    song = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND album = ?", name, album)
+    if !song.empty?
+      song_data = song[0]
+      song = Song.new(song_data[0], song_data[1], song_data[2])
+    else
+      song = self.create(name: name, album: album)
+    end
+    song
+  end 
 
 end
