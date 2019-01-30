87bb047b8db559dd606ef2145433320b4c05eb35
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-30 01:25:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -83,7 +83,7 @@ class Dog
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
-      binding.pry
+      # binding.pry
       dog_data = dog[0]
       self.find_by_name(dog_data[1])
       # dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
