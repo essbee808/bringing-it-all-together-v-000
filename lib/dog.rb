65b5f7fb7fa793f146beaf718d1d4311ad7e20ed
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-30 01:18:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -81,7 +81,6 @@ class Dog
   end
   
   def self.find_or_create_by(name:, breed:)
-    # binding.pry
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
       dog_data = dog[0]
